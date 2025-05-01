e6d29c80a5897d6926168ff0008cd9c8da9ee25d
marcossantanaioc <marcosvssantana@gmail.com>
2025-05-01 12:11:11 +0000

    Refactor: Align edge_indices with PyTorch Geometric format.
    Updates edge_indices to use a two-row structure for source and target nodes, consistent with PyTorch Geometric. Adds symmetrical edges, ensuring both (i, j) and (j, i) exist for each connection.

--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -97,16 +97,36 @@ class TestMPNNBatching:
         return _generate_random_dataset(num_examples=64)
 
     def test_create_batch_edge_index(self, batch):
+        """Test batch edge indices."""
         collated_edge_indices = data_utils.create_batch_edge_index(
             [dset.edge_indices for dset in batch.dsets]
         )
 
+        # Check number of edges match expected values for all elements.
         assert collated_edge_indices.size(1) == batch.total_edges
+
+        # Check node index match the number of nodes
+        assert collated_edge_indices.max().item() + 1 == batch.total_nodes
 
     def test_mpnn_collate_diag(self, batch):
         collated_dataset = data_utils.mpnn_collate_diag(batch.dsets)
 
+        # Check features have the right shape
         assert collated_dataset.atom_features.size(0) == batch.total_nodes
+        assert (
+            collated_dataset.atom_features.size(1)
+            == constants.NUM_ATOM_FEATURES
+        )
+        assert (
+            collated_dataset.bond_features.size(1)
+            == constants.NUM_BOND_FEATURES
+        )
+
+        # Check batch vector is correct
+        assert collated_dataset.batch_vector.size(0) == batch.total_nodes
+        assert torch.unique(collated_dataset.batch_vector).size(0) == len(
+            batch.dsets
+        )
 
 
 if __name__ == "__main__":
